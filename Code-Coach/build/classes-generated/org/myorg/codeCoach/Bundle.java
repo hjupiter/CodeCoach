10c4971f5348f53dc42868df0b844418cffea118
hjupiter <hector_franklin_17@yahoo.es>
2016-01-22 02:49:22 -0500

    Ventana de resultados

---
+++ Code-Coach/build/classes-generated/org/myorg/codeCoach/Bundle.java
@@ -0,0 +1,27 @@
+package org.myorg.codeCoach;
+/** Localizable strings for {@link org.myorg.codeCoach}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Result</i>
+     * @see ResultTopComponent
+     */
+    static String CTL_ResultAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_ResultAction");
+    }
+    /**
+     * @return <i>Result Window</i>
+     * @see ResultTopComponent
+     */
+    static String CTL_ResultTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_ResultTopComponent");
+    }
+    /**
+     * @return <i>This is a Result window</i>
+     * @see ResultTopComponent
+     */
+    static String HINT_ResultTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_ResultTopComponent");
+    }
+    private void Bundle() {}
+}
